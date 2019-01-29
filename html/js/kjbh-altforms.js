16c680a5df5d920da1641c90260e4656cbb9cf71
Scott Oglesby <soglesby@fastmail.net>
2019-01-28 23:03:22 -0800

    Added more relation types; URL query param support; fixed some radicals
    Improve explanation of each related kanji's connection to the main one
    Add manually discovered relations (symmetric and directional, such as alternative forms)
    Some refactoring in kjbh-logic
    Now handles ?k=X URL query param for specific kanji, bookmarking, etc.
    Patch dictionary to fix issues with mound and town radicals

---
+++ html/js/kjbh-altforms.js
@@ -0,0 +1,33 @@
+// related kanji where primary key is the more usual or standard choice
+var altforms = {
+	"陥": {
+		"落": {
+			"reason": "落ち入る (おちいる): other form of 陥る (to fall into)",
+		}
+	},
+	"披": {
+		"展": {
+			"reason": "展く (ひらく): other form of 披く (to open; to unfold; to spread out)",
+		}
+	},
+	"開": {
+		"披": {
+			"reason": "披く (ひらく, to open): similar to 開く (to open)",
+		}
+	},
+	"少": {
+		"寡": {
+			"reason": "寡し (すこし): other form of 少し (a little)",
+		}
+	},
+	"香": {
+		"薫": {
+			"reason": "薫り (かおり): other form of 香り (aroma, fragrance)",
+		}
+	},
+	"歳": {
+		"才": {
+			"reason": "才: 書き換え for 歳 (years, age)",
+		}
+	},		
+};
---
+++ html/js/kjbh-related.js
@@ -0,0 +1,55 @@
+var kankeiji = [
+    {
+		"kanji": [ "点", "付" ],
+        "reason": "点ける == 付ける (つける; to turn on a light)",
+    },
+	{
+		"kanji": ["旁", "啻", "啼", "楴", "榜", "蹄", "蒂", "滂", "蒡", "膀", "磅", "謗"],
+        "reason": "similar form but no single common component",
+    },
+	{
+		"kanji": ["解", "廨", "懈", "邂", "蟹", "蠏"],
+        "reason": "similar form but no single common component",
+        "comment": "can compose with first",
+    },
+	{
+		"kanji": ["懐", "壊"],
+        "reason": "similar form but no single common component",
+    },
+	{
+		"kanji": ["勧", "潅", "権", "歓", "観"],
+        "reason": "similar form but no single common component",
+    },
+	{
+		"kanji": ["隷", "款"],
+        "reason": "similar form but no single common component",
+    },
+	{
+		"kanji": ["陥", "焔"], 
+        "reason": "similar form but no single common component",
+    },
+	{
+		"kanji": ["刹", "殺"],  
+        "reason": "similar form but no single common component",
+    },
+	{
+		"kanji": ["謡", "揺"],   
+        "reason": "similar form but no single common component",
+    },
+	{
+		"kanji": ["危", "厄"],
+        "reason": "similar form but no single common component",
+    },
+	{
+		"kanji": ["侵", "浸", "寝", "寢", "駸"],
+        "reason": "similar form but no single common component",
+    },
+	{
+		"kanji": ["貴", "遣", "匱", "遺", "潰", "瞶"],
+        "reason": "similar form but no single common component",
+    },
+	{
+		"kanji": ["島", "鳥"],  
+        "reason": "similar form but no single common component",
+    },
+];
